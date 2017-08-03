6b2650f2140aee7b419fc5924e33c785a3b4344f
Klaus Pontoppidan <pontoppi@stsci.edu>
2017-08-03 11:45:20 -0400

    API changed, so update example

--- examples/hd163296_h2o.py
+++ examples/hd163296_h2o.py
@@ -4,9 +4,8 @@ from pyvisir import reduction,visir_run
 
 path = '/astro/pontoppi/DATA/VISIR2.0/095.C-0203'
 vrun = visir_run.visir_run(path)
-#scilist = vrun.get_sciencelist(1368832)
 scilist = vrun.get_sciencelist(1368835)
 stdlist = vrun.get_caliblist(1368838)
 
-Red = reduction.Reduction(scilist=scilist,stdlist=stdlist,
+Red = reduction.Reduction(scilist,stdlist,
                           level1=True, level2=False, level3=False,save_flat=False)
